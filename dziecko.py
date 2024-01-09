8f8d92b837f1be0536ab5dc110a5bc70522d3139
Katarzyna Lepek <kasia.gliszczynska@gmail.com>
2024-01-09 12:08:07 +0100

    Tworzenie funkcji dziecka

--- dziecko.py
+++ dziecko.py
@@ -1,2 +1,27 @@
+import datetime
+
 class Dziecko:
     _name = "Dziecko"
+    def __init__(self, imie, plec, data_urodzenia):
+        self.imie = imie
+        self.plec = plec
+        self.data_urodzenia = data_urodzenia
+
+    def dodaj_dziecko():
+        imie = input("Podaj imię dziecka:  ")  #zakładam, że można nazwać dziecko jakkolwiek się chce
+        
+        plec = input("Wybierz płeć dziecka: Dz/Ch    ")
+        plec = plec.lower()
+        while plec != ("dz" and "ch"):
+            plec = input("""Żle zdefiniowana płeć.
+                         Wybierz płeć dziecka: Dz/Ch    """)
+            plec = plec.lower()
+        
+        data_urodzenia = input("Podaj datę urodzenia dziecka w formacie DD-MM-RRRR: ")
+        while True:
+            try:
+                data_urodzenia = datetime.datetime.strptime(data_urodzenia, '%d-%m-%Y')
+                print("Wprowadzona data urodzenia:  ", data_urodzenia)
+                break
+            except ValueError:
+                data_urodzenia = input("Nieprawidłowa data. Upewnij się, że podajesz datę urodzenia w formacie DD-MM-RRRR:  ")
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-print ("""Witaj w Kontrolerze Wzrostu Dziecka!
-       Wybierz opcję z poniższego menu: 
-            1 - Dodaj dziecko
-            2 - Moje dzieci
-            3 - Wyjdź""")
+from dziecko import Dziecko
+
+while True:
+    print ("""Witaj w Kontrolerze Wzrostu Dziecka!
+           Wybierz opcję z poniższego menu: 
+                1 - Dodaj dziecko
+                2 - Moje dzieci
+                3 - Wyjdź
+           Wybieram:        """)
+    wybor = input()
+    if wybor == "1":
+        Dziecko.dodaj_dziecko()
+    elif wybor == "2":
+        print("Moje dzieci")
+    elif wybor == "3":
+        break
+    else:
+        print("Nieprawidłowy wybór. Spróbuj jeszcze raz.")
+print("Dziękuję, koniec programu")
 
